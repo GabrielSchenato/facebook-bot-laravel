672ba44943b16d2844e2339ab0350b448d57b9b0
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-06-22 11:22:30 -0300

    Model e migration de produtos

---
+++ app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $appends = ['image_full_url'];
+    protected $fillable = [
+        'title', 'subtitle', 'image_url', 'url'
+    ];
+    
+    protected function getImageFullUrlAttribute()
+    {
+        return 'https://s3-'.env('AWS_REGION').'.amazonaws.com/'.env('AWS_BUCKET').'/products/'.$this->attributes['image_url'];
+    }
+}
---
+++ database/migrations/2018_06_22_141609_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 45);
+            $table->string('subtitle', 100);
+            $table->string('image_url', 250);
+            $table->string('url', 250);
+            $table->timestamps();
+        });
+        
+        Schema::create('message_product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('message_id');
+            $table->integer('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('message_product');
+    }
+}
