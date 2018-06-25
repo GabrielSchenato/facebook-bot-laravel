94eb3836066e3e78638a34c26ef0bfb22ffe2f69
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-06-25 11:56:12 -0300

    Integrando no Bot templates

--- app/Repositories/MessagesBuilderRepository.php
+++ app/Repositories/MessagesBuilderRepository.php
@@ -2,8 +2,10 @@
 
 namespace App\Repositories;
 
-use CodeBot\Bot;
 use App\Message;
+use CodeBot\Bot;
+use CodeBot\Element\Button;
+use CodeBot\Element\Product;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -22,10 +24,38 @@ class MessagesBuilderRepository
     public function createMessage(Bot $bot, Message $message)
     {
         if ($message->template) {
-            
+            call_user_func_array([$this, $message->type], [$bot, $message]);
         } else {
             $bot->message($message->type, $message->message);
         }
+    }
+
+    private function buttons(Bot $bot, Message $message)
+    {
+        $elements = [];
+
+        foreach ($message->elements as $element) {
+            $config = [];
+            if ($element->webview_height_ratio) {
+                $config['webview_height_ratio'] = $element->webview_height_ratio;
+            }
+            $elements[] = new Button($element->type, $element->title, $element->postback, $config);
+        }
+
+        $bot->template($message->type, $message->message, $elements);
+    }
+
+    private function generic(Bot $bot, Message $message)
+    {
+        $products = [];
+
+        foreach ($message->$products as $product) {
+            $default_action = new Button('web_url', null, $product->url);
+            $products[] = new Product($product->title, $product->image_full_url, $product->subtitle, $default_action);
+        }
+
+        $bot->message('text', $message->message);
+        $bot->template($message->type, $message->message, $products);
     }
 
 }
