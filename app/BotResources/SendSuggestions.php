6cf4ab31b75f9894483c350f4155e068ece941da
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-06-25 15:30:55 -0300

    Criando o recurso personalizado

---
+++ app/BotResources/SendSuggestions.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\BotResources;
+
+use App\Repositories\SuggestionsRepository;
+use CodeBot\Bot;
+use CodeBot\Resources\ResourceInterface;
+use CodeBot\SenderRequest;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SendSuggestions
+ *
+ * @author gabriel
+ */
+class SendSuggestions implements ResourceInterface
+{
+
+    public function __invoke(SenderRequest $sender, Bot $bot): bool
+    {
+        $suggestion = new SuggestionsRepository;
+
+        if ($suggestion->statusCheck($sender->getSenderId()) === 'started') {
+            $suggestion->create($sender->getSenderId(), $sender->getMessage());
+
+            $bot->message('text', 'Obrigado, sua sugestão foi enviada com sucesso!');
+            $suggestion->statusStop($sender->getSenderId());
+
+            return true;
+        }
+        return false;
+    }
+
+    public function statusStart(SenderRequest $sender, Bot $bot)
+    {
+        (new SuggestionsRepository())->statusStop($sender->getSenderId());
+        $bot->message('text', 'Ok, por favor, qual sua sugestão?');
+        $bot->message('text', 'Você pode digitar, mas envie a sua sugestão em uma única mensagem!');
+    }
+
+}
--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+SendSuggestions;
+
+use App\BotResources\SendSuggestions;
 use App\Postback;
 use App\Repositories\MessagesBuilderRepository;
 use CodeBot\Build\Solid;
+use CodeBot\Resources\Resolver;
 use CodeBot\SenderRequest;
 use CodeBot\WebHook;
 use Illuminate\Http\Request;
@@ -29,17 +33,34 @@ class BotController extends Controller
         $sender = new SenderRequest();
         $senderId = $sender->getSenderId();
         $postback = $sender->getPostBack();
-        
+
         $bot = Solid::factory();
         Solid::setPageAccessToken(config('botfb.pageAccessToken'));
         Solid::setSenderId($senderId);
-        
-        $postback = Postback::where('value',$postback)->first();
-        
+
+        if ($postback === 'suggestion') {
+            (new SendSuggestions)->statusStart($sender, $bot);
+            return '';
+        }
+
+        $postback = Postback::where('value', $postback)->first();
+
+        if (!$postback) {
+            $botResourcesResolver = new Resolver;
+            $botResourcesResolver->register(SendSuggestions::class);
+
+            if ($botResourcesResolver->resolver($sender, $bot)) {
+                return '';
+            }
+            $bot->message('text', 'Desculpe, eu não sei o que você quis dizer ):');
+            $bot->message('text', 'Use o menu ao lado esquerdo do campo aonde você escreve para ver o que você pode fazer');
+            return '';
+        }
+
         foreach ($postback->messages as $message) {
             (new MessagesBuilderRepository)->createMessage($bot, $message);
         }
-        
+
         return '';
     }
 
